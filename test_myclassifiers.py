497250a14a01b12352dc66a861f698fbae33bc8e
Emily Inkrott <einkrott@zagmail.gonzaga.edu>
2022-05-05 02:32:35 +0000

    okay now the tests are in the right file. Whoops!

--- test_myclassifiers.py
+++ test_myclassifiers.py
@@ -786,7 +786,6 @@ def test_decision_tree_classifier_predict():
     dt.print_decision_rules()
 
 def test_random_forest_fit():
-    # interview dataset
     header = ["level", "lang", "tweets", "phd", "interviewed_well"]
     X_train = [
         ["Senior", "Java", "no", "no"],
@@ -806,85 +805,86 @@ def test_random_forest_fit():
     ]
     y_train = ["False", "False", "True", "True", "True", "False", "True", "False", "True", "True", "True", "True", "True", "False"]
 
-    # tree 1 uses attributes level (att0) and phd (att3)
-    tree_1 = ['Attribute', 'att0',
-                ['Value', 'Junior',
-                    ['Attribute', 'att1',
-                        ['Value', 'Python',
-                                ['Leaf', 'True', 3, 4]
-                        ], 
-                        ['Value', 'R', 
-                            ['Leaf', 'False', 1, 4]
-                        ]
-                    ]
-                ],
-                ['Value', 'Mid',
-                    ['Leaf', 'True', 4, 10]
-                ],
-                ['Value', 'Senior', 
-                    ['Leaf', 'True', 2, 10]
-                ]
-            ]
 
-    #tree 2 uses attributes level (att0) and lang (att1)
-    tree_2 = ['Attribute', 'att2',
-                ['Value', 'no', 
-                    ['Leaf', 'True', 6, 10]
-                ], 
-                ['Value', 'yes', 
-                    ['Attribute', 'att3', 
-                        ['Value', 'no', 
-                            ['Leaf', 'True', 2, 4]
-                        ],
-                        ['Value', 'yes', 
-                            ['Leaf', 'False', 2, 4]
-                        ]
-                    ]
-                ]
-            ]
-
-    #tree 3 uses attributes tweets (att2) and phd (att3)
-    tree_3 = ["Attribute", "att2",
-                ["Value", "no",
-                    ["Attribute", "att3",
-                        ["Value", "no",
-                            ["Leaf", "False", 4, 7]
-                        ],
-                        ["Value", "yes", 
-                            ["Leaf", "False", 3, 7]
-                        ]
-                    ]
-                ],
-                ["Value", "yes",
-                    ["Attribute", "att3",
-                        ["Value", "no",
-                            ["Leaf", "True", 4, 7]
-                        ],
-                        ["Value", "yes", 
-                            ["Leaf", "True", 3, 7]
-                        ]
-                    ]
-                ]
-            ]
-    trees = [tree_1, tree_2, tree_3]
-
-    #np.random.seed(2)
     random_state = 2
     N = 3
     M = 2
     F = 2
-    print(np.random.randint(0,4, size=F)) #tree 1 will use level and phd
-    print(np.random.randint(0,4, size=F)) #tree 2 will ue lang and level
-    print(np.random.randint(0,4, size=F)) #tree 3 will use tweets and phd
+
+    tree_1 = ['Attribute', 'att0', 
+            ['Value', 'Junior', 
+                    ['Leaf', 'True', 1, 10]
+                ], 
+                ['Value', 'Mid', 
+                    ['Leaf', 'True', 6, 10]
+                ], 
+                ['Value', 'Senior', 
+                    ['Attribute', 'att2', 
+                        ['Value', 'no', 
+                            ['Leaf', 'False', 2, 3]
+                        ], 
+                        ['Value', 'yes', 
+                            ['Leaf', 'True', 1, 3]
+                        ]
+                    ]
+                ]
+            ]
+
+    tree_2 = ['Attribute', 'att0', 
+                ['Value', 'Junior', 
+                    ['Leaf', 'True', 1, 10]
+                ], 
+                ['Value', 'Mid', 
+                    ['Leaf', 'True', 6, 10]
+                ], 
+                ['Value', 'Senior', 
+                    ['Attribute', 'att3', 
+                        ['Value', 'no', 
+                            ['Leaf', 'False', 1, 3]
+                        ], 
+                        ['Value', 'yes', 
+                            ['Leaf', 'False', 2, 3]
+                        ]
+                    ]
+                ]
+            ]
+
+    trees = [tree_1, tree_2]
 
     rf = MyRandomForestClassifier(N, M, F, random_state=2)
     rf.fit(X_train, y_train)
     for tree in rf.random_forest:
-        print(tree.tree)
-        print()
-        # assert tree.tree in trees
-    
-    assert False is True
+        assert tree.tree in trees
 
 def test_random_forest_predict():
-    assert False is True
+    header = ["level", "lang", "tweets", "phd", "interviewed_well"]
+    X_train = [
+        ["Senior", "Java", "no", "no"],
+        ["Senior", "Java", "no", "yes"],
+        ["Mid", "Python", "no", "no"],
+        ["Junior", "Python", "no", "no"],
+        ["Junior", "R", "yes", "no"],
+        ["Junior", "R", "yes", "yes"],
+        ["Mid", "R", "yes", "yes"],
+        ["Senior", "Python", "no", "no"],
+        ["Senior", "R", "yes", "no"],
+        ["Junior", "Python", "yes", "no"],
+        ["Senior", "Python", "yes", "yes"],
+        ["Mid", "Python", "no", "yes"],
+        ["Mid", "Java", "yes", "no"],
+        ["Junior", "Python", "no", "yes"]
+    ]
+    y_train = ["False", "False", "True", "True", "True", "False", "True", "False", "True", "True", "True", "True", "True", "False"]
+
+    X_test= [["Junior", "Java", "yes", "no"], ["Junior", "Java", "yes", "yes"]]
+    y_test = ['True', 'True']
+
+    random_state = 2
+    N = 3
+    M = 2
+    F = 2
+    rf = MyRandomForestClassifier(N, M, F, random_state=2)
+    rf.fit(X_train, y_train)
+    predictions = rf.predict(X_test)
+    for i in range(len(predictions)):
+        assert predictions[i] == y_test[i]
